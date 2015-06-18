18508da61d9bf3d0da24ad86e1ae14f24a9ddd32
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2015-06-18 08:30:13 -0500

    begin resume home page

--- app/app.js
+++ app/app.js
@@ -4,11 +4,13 @@ var app = angular.module('ryanWeb', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/canvas/:page', {
-      templateUrl: 'views/canvas.html'
+    templateUrl: 'views/canvas.html'
   }).when('/shopping', {
     templateUrl: 'views/shoppingList.html'
+  }).when('/home', {
+    templateUrl: 'views/home.html'
   }).otherwise({
-    redirectTo: '/canvas/particles'
+    redirectTo: '/home'
   })
 }]);
 
